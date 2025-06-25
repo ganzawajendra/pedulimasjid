2c6f5328645214c758e7dd2a429eeb08c68bdf87
ganzawajendra <ganzawjendra@gmail.com>
2025-06-25 15:50:37 +0700

    api masjid

--- src/components/fragments/CardMasjid.jsx
+++ src/components/fragments/CardMasjid.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CardMasjid = () => {
+const CardMasjid = ({name, address, status, demand, created_at, url}) => {
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300">
       <div className="bg-gray-400 h-32 shadow-lg">
         <img
-          src="/images/hero.png"
-          alt="tes"
+          src={url}
+          alt={name}
           className="w-full h-full object-cover"
         />
       </div>
       <div className="p-4 md:p-6">
         <div className="flex justify-between items-start">
-          <h3 className="text-xl font-semibold text-gray-800">Masjid Al-Fatih</h3>
+          <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
           <span className="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full">
-            Terverifikasi
+            {status}
           </span>
         </div>
 
         <div className="flex items-center gap-1 mb-3">
           <i className="fa-solid fa-location-dot text-gray-500 text-sm"></i>
-          <p className="text-sm text-gray-500">tes</p>
+          <p className="text-sm text-gray-500">{address}</p>
         </div>
 
         <div className="">
           <h4 className="text-sm font-medium text-gray-800">
             Kebutuhan:
           </h4>
-          <p className="line-clamp-3 overflow-hidden w-full text-xs text-ellipsis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid impedit nesciunt, eos blanditiis consequatur veritatis nisi similique tempora neque, magnam ipsam laborum cumque unde qui, quasi pariatur quae! Velit eveniet dignissimos, tempore itaque eos facere pariatur maiores animi mollitia architecto voluptatum fuga voluptatem ratione nam dolore ipsa eaque doloremque perferendis debitis explicabo illo? Perspiciatis tempora nisi non veniam itaque nulla quibusdam, mollitia vitae facilis deleniti, delectus accusamus ratione ex nam! Voluptatum nisi omnis alias dicta eligendi? Odio, culpa reprehenderit dolor laudantium magni, ad incidunt sint amet et assumenda porro quos quia a saepe maxime alias quae quaerat quis veritatis natus.</p>
+          <p className="line-clamp-3 overflow-hidden w-full text-xs text-ellipsis">{demand}</p>
         </div>
 
         <div className="flex justify-between items-center pt-3 md:pt-4 border-t border-gray-100">
@@ -36,7 +36,7 @@ const CardMasjid = () => {
           <h4 className="text-sm font-medium text-gray-800">
             Terdaftar:
           </h4>
-          <p className="text-xs text-gray-800">2020-20-20 20:20</p>
+          <p className="text-xs text-gray-800">{created_at}</p>
           </div>
           <Link
             to="/card-detail"
--- src/components/layouts/HomeLayout.jsx
+++ src/components/layouts/HomeLayout.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FormAddPengurus from "../fragments/FormAddPengurus";
 import HeroImage from "/images/hero.png";
 import CardDeskripsiHome from "../fragments/CardDeskripsiHome";
 import ListCard from "../elements/ListCard";
 import CardMasjid from "../fragments/CardMasjid";
+import axios from "axios";
+import dataMasjidApi from "../../api/masjid.json";
 
 const HomeLayout = ({ donasi, masjid, donatur }) => {
+  // Jika menggunakan API aktifkan ini
+  // const urlApi = "http://localhost:8000/masjid";
+
+  const [dataMasjid, setDatamasjid] = useState([]);
+
+  async function getAllData() {
+    // Jika menggunakan API aktifkan ini
+    // const response = await axios.get(urlApi);
+    
+    setDatamasjid(dataMasjidApi.masjid);
+  }
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+
   return (
     <>
       {/* Hero Section */}
       <div
         className={`bg-[url('/images/hero.png')] bg-contain bg-no-repeat bg-center h-90 flex flex-col items-start px-20 justify-center text-start`}
       >
-        <h1 className="text-5xl/15 text-gray-800 w-200">Donasi Masjid Mudah dan Cepat di Peduli Masjid</h1>
-        <p className="text-xl my-2 text-gray-500">Temukan informasi donasi masjid dengan mudah dan cepat</p>
+        <h1 className="text-5xl/15 text-gray-800 w-200">
+          Donasi Masjid Mudah dan Cepat di Peduli Masjid
+        </h1>
+        <p className="text-xl my-2 text-gray-500">
+          Temukan informasi donasi masjid dengan mudah dan cepat
+        </p>
       </div>
 
       {/* Deskripsi Singkat */}
       <div className="w-full flex flex-col items-center justify-center text-center mt-10">
-        <h2 className="text-2xl font-semibold text-gray-800">Bantu Wujudkan Masjid Layak untuk Umat</h2>
-        <p className="text-md text-gray-500">Platform donasi online transparan untuk pembangunan & renovasi masjid
-          kurang mampu di seluruh Indonesia.</p>
+        <h2 className="text-2xl font-semibold text-gray-800">
+          Bantu Wujudkan Masjid Layak untuk Umat
+        </h2>
+        <p className="text-md text-gray-500">
+          Platform donasi online transparan untuk pembangunan & renovasi masjid
+          kurang mampu di seluruh Indonesia.
+        </p>
       </div>
 
       {/* Fungsi */}
@@ -29,9 +55,19 @@ const HomeLayout = ({ donasi, masjid, donatur }) => {
       </div>
 
       {/* Card Donasi */}
-      <div class="w-full border border-gray-300 shadow-lg grid grid-cols-1 md:grid-cols-5 gap-5 mt-10 rounded-lg p-4 md:p-10">
-        <div class="col-span-1 md:col-span-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 order-3 md:order-none">
-          <CardMasjid />
+      <div className="w-full border border-gray-200 shadow-lg  gap-5 mt-10 rounded-lg p-4">
+        <div className=" grid grid-cols-4 gap-5">
+          {dataMasjid.map((item, index) => (
+            <CardMasjid
+              key={index}
+              name={item.name}
+              address={item.address}
+              status={item.status}
+              demand={item.demand}
+              created_at={item.created_at}
+              url={item.url}
+            />
+          ))}
         </div>
       </div>
 
@@ -39,25 +75,35 @@ const HomeLayout = ({ donasi, masjid, donatur }) => {
       <div className="w-full grid grid-cols-3 mt-10 px-20  py-10">
         <div className="border-l border-gray-800 flex flex-col items-center justify-center p-6">
           <h4 className="text-md text-gray-500">Total Donasi</h4>
-          <h3 className="text-lg font-semibold text-gray-800">Rp <span>{donasi}</span></h3>
+          <h3 className="text-lg font-semibold text-gray-800">
+            Rp <span>{donasi}</span>
+          </h3>
         </div>
         <div className="border-r border-l border-gray-800 flex flex-col items-center justify-center p-6">
           <h4 className="text-md text-gray-500">Terbantu</h4>
-          <h3 className="text-lg font-semibold text-gray-800"><span>{masjid}</span> Masjid</h3>
+          <h3 className="text-lg font-semibold text-gray-800">
+            <span>{masjid}</span> Masjid
+          </h3>
         </div>
         <div className="border-r border-gray-800 flex flex-col items-center justify-center p-6">
           <h4 className="text-md text-gray-500">Lebih dari</h4>
-          <h3 className="text-lg font-semibold text-gray-800"><span>{donatur}</span> Donatur</h3>
+          <h3 className="text-lg font-semibold text-gray-800">
+            <span>{donatur}</span> Donatur
+          </h3>
         </div>
       </div>
 
       {/* Open Akun Pengurus */}
       <div className="bg-neutral-200 rounded-lg p-8 mt-10 grid grid-cols-3 gap-5">
         <div className="col-span-2 p-6">
-          <h2 className="text-2xl font-semibold text-gray-800">Jadi Pengurus Masjid?</h2>
-          <p className="text-md text-gray-500">Daftarkan masjid Anda untuk mendapatkan bantuan donasi dari
+          <h2 className="text-2xl font-semibold text-gray-800">
+            Jadi Pengurus Masjid?
+          </h2>
+          <p className="text-md text-gray-500">
+            Daftarkan masjid Anda untuk mendapatkan bantuan donasi dari
             masyarakat. Kami akan membantu memverifikasi dan mempromosikan
-            kebutuhan masjid Anda.</p>
+            kebutuhan masjid Anda.
+          </p>
           <ul className="space-y-2 text-gray-700">
             <ListCard icon="fa-solid fa-check" style="text-green-500">
               Kelola donasi dengan transparan
